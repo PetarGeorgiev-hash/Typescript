781ba74b0732ca5672455e61832241ead2a1cb43
Petar Georgiev <petiyo07@abv.bg>
2023-12-05 14:39:55 +0200

    first commit

---
+++ build/arrays.js
@@ -0,0 +1,6 @@
+"use strict";
+const carMakers = ["ford", "toyota", "chevy"];
+const car_Makers = [];
+const dates = [new Date(), new Date()];
+const carsByMake = [["f150"], ["corolla"], ["camaro"]];
+const cars_By_Make = [];
---
+++ src/arrays.ts
@@ -0,0 +1,7 @@
+const carMakers: string[] = ["ford", "toyota", "chevy"];
+const car_Makers: string[] = [];
+const dates = [new Date(), new Date()];
+
+const carsByMake = [["f150"], ["corolla"], ["camaro"]];
+const cars_By_Make: string[][] = []
+
